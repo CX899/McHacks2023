7e2efd51439d356dcaa91865309e571146aeac41
Thanh-Huy1104 <etienne01.nguyen@gmail.com>
2023-01-28 21:40:43 -0500

    added firebase firestore

--- dashboard-app/src/scenes/progress/index.jsx
+++ dashboard-app/src/scenes/progress/index.jsx
@@ -1,7 +1,8 @@
 import Header from '../../components/Header'
-import {Box, Typography} from '@mui/material'
+import {Box, Typography, Button} from '@mui/material'
 import Navbar from '../global/navbar'
 import Leftbar from '../global/leftbar'
+import {doc, getDoc} from 'firebase/firestore'
 
 const Progress = () => {
   return (
@@ -11,6 +12,9 @@ const Progress = () => {
     <Navbar/>
     <Box m="60px">
         <Header title="Progress" subtitle="Keep track of progress"/>
+    </Box>
+    <Box m="60px">
+        <Button />
     </Box>
     </div>
     </div>
