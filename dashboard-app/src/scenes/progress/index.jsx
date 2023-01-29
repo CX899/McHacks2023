efdc6de201f5c8e608993df7673fd0cc93e962a7
Thanh-Huy1104 <etienne01.nguyen@gmail.com>
2023-01-28 22:16:27 -0500

    remove

--- dashboard-app/src/scenes/progress/index.jsx
+++ dashboard-app/src/scenes/progress/index.jsx
@@ -2,9 +2,22 @@ import Header from '../../components/Header'
 import {Box, Typography, Button} from '@mui/material'
 import Navbar from '../global/navbar'
 import Leftbar from '../global/leftbar'
-import {doc, getDoc} from 'firebase/firestore'
+import {doc, setDoc} from 'firebase/firestore'
+import {db} from '../../firebase'
 
 const Progress = () => {
+
+
+  const handleAdd = async(e) => {
+    e.preventDefault()
+    await setDoc(doc(db, "users", "alovelace"), {
+      first: "Ada",
+      last: "Lovelace",
+      born: 1815
+    });
+
+
+  }
   return (
     <div className='app'>
     <Leftbar/>
@@ -14,7 +27,8 @@ const Progress = () => {
         <Header title="Progress" subtitle="Keep track of progress"/>
     </Box>
     <Box m="60px">
-        <Button />
+      <Typography variant="h4">Progress</Typography>
+        <Button onClick={handleAdd}/>
     </Box>
     </div>
     </div>
