74339ef4276c952f383097039692a646a3f1d361
Colin Xiong <xiong.colin899@gmail.com>
2023-01-28 21:32:06 -0500

    Fix Modal and addEvent button

--- dashboard-app/src/scenes/Modal/index.js
+++ dashboard-app/src/scenes/Modal/index.js
@@ -1,5 +1,8 @@
 import { motion } from 'framer-motion';
 import Backdrop from '../Backdrop';
+import { useState } from 'react';
+import { TextField, Button } from '@mui/material';
+
 
 const flip = {
     hidden: {
@@ -25,7 +28,16 @@ const flip = {
     },
   };
 
-const Modal = ({handleClose}) => {
+const Modal = ({handleClose, sendValues}) => {
+    const [input1, setInput1] = useState("");
+    const [input2, setInput2] = useState("");
+
+    const setValues = (e) => {
+        e.preventDefault();
+        console.log(input1, input2);
+        sendValues(input1, input2)
+    };  
+
     return (
         <Backdrop onClick={handleClose}>
             <motion.div 
@@ -36,9 +48,24 @@ const Modal = ({handleClose}) => {
             animate='visible'
             exit='visible'
             >
+              <form onSubmit={setValues}>
+                  <TextField
+                    label="Title"
+                    value={input1}
+                    onChange={(e) => setInput1(e.target.value)}
+                  />
+
+                  <TextField
+                    label="Enter a date in YYYY-MM-DD format"
+                    value={input2}
+                    onChange={(e) => setInput2(e.target.value)}
+                  />
+                  
+                  <Button type="submit"  onClick={handleClose}>Add Event</Button>
+                </form>
             </motion.div>
         </Backdrop>
     )
-}
+    }
 
 export default Modal;
--- dashboard-app/src/scenes/calendar/index.jsx
+++ dashboard-app/src/scenes/calendar/index.jsx
@@ -5,12 +5,39 @@ import Leftbar from '../global/leftbar'
 import FullCalendar from '@fullcalendar/react' 
 import dayGridPlugin from '@fullcalendar/daygrid' 
 import Modal from '../Modal/index.js';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 const Calendar = () => {
   const [modalOpen, setModalOpen] = useState(false);
-  const close = () => setModalOpen(false);
+  const close = () => {setTimeout(() => { setModalOpen(false);}, 1)};
   const open = () => setModalOpen(true);
+  const [input1, setInput1] = useState(null);
+  const [input2, setInput2] = useState(null);
+  const calendarRef = useRef(null);
+
+  const receivedValues = (input1, input2) => {
+    const calendar = calendarRef.current.calendar;
+    setInput1(input1);
+    setInput2(input2);
+    var date = new Date(input2 + 'T00:00:00');
+    console.log(date);
+
+    const newEvent = {
+      title: input1,
+      start: date,
+      allDay: true
+    }
+
+    if (!isNaN(date.valueOf())) {
+      calendar.addEvent(newEvent);
+  
+      //alert('Great. Now, update your database...');
+      // call your database and update it add the event objects in here
+  
+    } else {
+      alert('Invalid date.');
+    }
+  }
 
   const handleOpen = () => {
     (modalOpen ? close() : open());
@@ -24,6 +51,7 @@ const Calendar = () => {
     <Box m="60px">
         <Header title="Calendar" subtitle="Always know when thing's happen"/>
         <FullCalendar
+        ref={calendarRef}
         initialView="dayGridMonth"
         headerToolbar={{
           center: 'addEventButton',
@@ -40,7 +68,7 @@ const Calendar = () => {
         plugins={[ dayGridPlugin ]}
         height="auto"
         />
-        {modalOpen && <Modal modalOpen={modalOpen} handleClose={close}/>}
+        {modalOpen && <Modal modalOpen={modalOpen} handleClose={close} sendValues={receivedValues}/>}
     </Box>
     </div>
     </div>
